af86351d027751fe69ee4320181ce4a3018e2773
Andras Belicza <iczaaa@gmail.com>
2018-11-12 13:40:52 +0100

    Improve docs

--- shutdown.go
+++ shutdown.go
@@ -1,6 +1,6 @@
 /*
 
-Package shutdown aids graceful termination of goroutines on app shutdown.
+Package shutdown helps controlling app shutdown and graceful termination of goroutines.
 
 It listens for SIGTERM (e.g. kill command) and SIGINT (e.g. CTRL+C) signals,
 and also provides a manual way to trigger shutdown.
